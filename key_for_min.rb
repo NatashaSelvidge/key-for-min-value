d1f35d93580b655424e81910bd0332b2964bb108
Natasha Selvidge <Natasha.selvidge@gmail.com>
2020-11-16 08:58:30 -0800

    commit tree f51bed5cff711ce92fe255bdacc910e1a42abbd6

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,6 +1,5 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
-
 def key_for_min_value(name_hash)
 lowest_value = 1000
     lowest_key = ""
@@ -16,3 +15,5 @@ lowest_value = 1000
        return lowest_key
     end
   end 
+
+
